e057be835a5e968399f4911c8a9663bc09d258fd
Munyangabo junior <Munyangabo4@gmail.com>
2024-05-27 20:18:07 +0200

    second commit

--- client/src/components/Reset.js
+++ client/src/components/Reset.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Reset() {
-  return <div></div>;
+  return <div>reset</div>;
 }
--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -5,8 +5,11 @@ export default function Username() {
     <div className="container mx-auto">
       <div className="flex justify-center items-center h-screen">
         <div>
-          <div className="title">
-            <h4>Hello again</h4>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Hello again!</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              Explore More by connecting with us
+            </span>
           </div>
         </div>
       </div>
