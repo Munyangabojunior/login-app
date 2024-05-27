11668445eddf0e4eecb83c9756be3f460ffbc0ac
Munyangabo junior <Munyangabo4@gmail.com>
2024-05-27 19:36:55 +0200

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,53 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
+/** import all components **/
+
+import Username from "./components/Username.js";
+import Password from "./components/Password.js";
+import Register from "./components/Register.js";
+import Profile from "./components/Profile.js";
+import Recovery from "./components/Recovery.js";
+import Reset from "./components/Reset.js";
+import PageNotFound from "./components/PageNotfound.js";
+
+/** root routes**/
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Username></Username>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+  {
+    path: "/password",
+    element: <Password></Password>,
+  },
+  {
+    path: "/profile",
+    element: <Profile></Profile>,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery></Recovery>,
+  },
+  {
+    path: "/reset",
+    element: <Reset></Reset>,
+  },
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>,
+  },
+]);
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
-
-export default App;
---
+++ client/src/components/Username.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Username() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div>
+          <div className="title">
+            <h4>Hello again</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
